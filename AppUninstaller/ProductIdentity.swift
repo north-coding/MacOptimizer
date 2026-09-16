ea50a4b4f582b04dbdf905ac4b72d9292c03af71
north-coding <north.hu@gmail.com>
2026-09-16 20:42:39 +0800

    Normalize legacy product names

--- AppUninstaller/ProductIdentity.swift
+++ AppUninstaller/ProductIdentity.swift
@@ -2,4 +2,17 @@ import Foundation
 
 enum ProductIdentity {
     static let displayName = "MacOptimizer"
+
+    private static let legacyDisplayNames = [
+        "Mac优化大师",
+        "Mac最佳化大師",
+        "Macオプティマイザー",
+        "Mac 최적화 도구"
+    ]
+
+    static func normalizingLegacyProductName(in text: String) -> String {
+        legacyDisplayNames.reduce(text) { result, legacyName in
+            result.replacingOccurrences(of: legacyName, with: displayName)
+        }
+    }
 }
